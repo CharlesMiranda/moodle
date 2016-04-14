27dca9c99c9419ae2262293a9a6102c0ff3d7ecb
charles <cjlmiranda.2@gmail.com>
2016-04-14 10:16:43 -0300

    primeiro commit

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$isDesenvolvimento = false;
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = ($isDesenvolvimento) ? 'root' : '';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://localhost/moodle';
+$CFG->dataroot  = ($isDesenvolvimento) ? '/var/www/html/moodledata' : 'C:\\xampp\\moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
